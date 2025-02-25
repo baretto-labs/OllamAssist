cbd752855703a62555748469177833e140af64f4
mehdi-elke <m.elketroussi@gmail.com>
2025-02-25 20:46:41 +0100

    clean code

--- src/main/java/fr/baretto/ollamassist/chat/askfromcode/OverlayPromptPanelFactory.java
+++ src/main/java/fr/baretto/ollamassist/chat/askfromcode/OverlayPromptPanelFactory.java
@@ -1,10 +1,7 @@
 package fr.baretto.ollamassist.chat.askfromcode;
 
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.actionSystem.IdeActions;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
-import com.intellij.openapi.editor.actionSystem.EditorActionManager;
 import com.intellij.openapi.editor.event.SelectionEvent;
 import com.intellij.openapi.editor.event.SelectionListener;
 import com.intellij.openapi.editor.ex.EditorEx;
@@ -15,7 +12,6 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
@@ -24,9 +20,8 @@ import java.awt.event.KeyEvent;
 public class OverlayPromptPanelFactory {
 
     private static EditorActionHandler originalEnterHandler;
-    private static boolean isHandlingEnter = false;
 
-    public static PromptPanel showOverlayPromptPanel(Editor editor, int startOffset) {
+    public static void showOverlayPromptPanel(Editor editor, int startOffset) {
         int offset = editor.getDocument().getLineStartOffset(startOffset);
 
         PromptPanel panel = createOverlayPromptPanel(editor);
@@ -64,38 +59,6 @@ public class OverlayPromptPanelFactory {
                 panel.setVisible(false);
             }
         }, panel);
-       // overrideEditorEnterHandler(panel);
-        return panel;
-    }
-
-    private static void overrideEditorEnterHandler(PromptPanel panel) {
-        EditorActionManager actionManager = EditorActionManager.getInstance();
-        originalEnterHandler = actionManager.getActionHandler(IdeActions.ACTION_EDITOR_ENTER);
-
-        actionManager.setActionHandler(IdeActions.ACTION_EDITOR_ENTER, new EditorActionHandler() {
-            @Override
-            public void execute(@NotNull Editor editor, DataContext dataContext) {
-                if (isHandlingEnter) {
-                    return;
-                }
-                isHandlingEnter = true;
-                try {
-                    Component focusOwner = KeyboardFocusManager.getCurrentKeyboardFocusManager().getFocusOwner();
-                    if (SwingUtilities.isDescendingFrom(focusOwner, panel) && panel.isVisible()) {
-                        String selectedText = editor.getSelectionModel().getSelectedText();
-                        panel.triggerAction(new AskFromCodeAction(editor, panel, selectedText));
-
-                        SwingUtilities.invokeLater(() ->
-                                editor.getSelectionModel().removeSelection()
-                        );
-                    } else {
-                        originalEnterHandler.execute(editor, dataContext);
-                    }
-                } finally {
-                    isHandlingEnter = false;
-                }
-            }
-        });
     }
 
     private static @NotNull PromptPanel createOverlayPromptPanel(Editor editor) {
